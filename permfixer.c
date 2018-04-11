d2b598a122914eda16256814cdbde549b28fa709
Tom Richards <tom@tomrichards.net>
2018-04-11 12:54:48 -0400

    Check for chmod/chown errors

--- permfixer.c
+++ permfixer.c
@@ -21,19 +21,32 @@ const mode_t file_perm = 0664;
 static inline void permfixer_fix_file(const char *path)
 {
     // TODO(tom): POSIX ACLs
-    chown(path, user_owner, group_owner);
+    if (chown(path, user_owner, group_owner) == -1) {
+        fprintf(stderr, "Error changing ownership of %s: %s\n", path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     // Attempt to not clobber files with higher permission levels
     struct stat statbuf;
     stat(path, &statbuf);
-    chmod(path, statbuf.st_mode | file_perm);
+    if (chmod(path, statbuf.st_mode | file_perm) == -1) {
+        fprintf(stderr, "Error changing permissions of %s: %s\n", path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 }
 
 static inline void permfixer_fix_dir(const char *path)
 {
     // TODO(tom): POSIX ACLs
-    chown(path, user_owner, group_owner);
-    chmod(path, dir_perm);
+    if (chown(path, user_owner, group_owner) == -1) {
+        fprintf(stderr, "Error changing ownership of %s: %s\n", path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (chmod(path, dir_perm) == -1) {
+        fprintf(stderr, "Error changing permissions of %s: %s\n", path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 }
 
 static int permfixer_process(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
@@ -62,16 +75,19 @@ int main(int argc, char *argv[])
 
     path = realpath(argv[1], NULL);
     if (path == NULL) {
-        perror("Failed to resolve path");
+        fprintf(stderr, "Failed to resolve path %s: %s\n", argv[1], strerror(errno));
         exit(EXIT_FAILURE);
     }
-    printf("Fixing %s\n", path);
+
+    printf("Fixing %s... ", path);
 
     permfixer_fix_dir(path);
     if (nftw(path, permfixer_process, 20, flags) == -1) {
         perror("NFTW");
         exit(EXIT_FAILURE);
     }
+
+    puts("done");
 
     exit(EXIT_SUCCESS);
 }
